105e50e926c21459635e9d208fe5a0d65329d441
alvaro garcia <derecerca@gmail.com>
2016-05-20 00:29:26 +0200

    refactor: DRY

--- src/main/java/com/codurance/test/quakker/core/rules/Subscribing.java
+++ src/main/java/com/codurance/test/quakker/core/rules/Subscribing.java
@@ -4,6 +4,7 @@ import com.codurance.test.quakker.core.ports.QuakkRepository;
 import com.codurance.test.quakker.core.domain.User;
 
 public class Subscribing implements Rule {
+	public static final String TOKEN = " follows ";
 	private final QuakkRepository repository;
 
 	public Subscribing (final QuakkRepository repository) {
@@ -12,7 +13,7 @@ public class Subscribing implements Rule {
 
 	@Override
 	public void apply (final String representation) {
-		final String[] values = representation.split(" follows ");
+		final String[] values = representation.split(TOKEN);
 		final User whoSubscribes = new User(values[0]);
 		final User subscriptionTo = new User(values[1]);
 		repository.subscribe(whoSubscribes, subscriptionTo);
@@ -20,6 +21,6 @@ public class Subscribing implements Rule {
 
 	@Override
 	public boolean appliesTo (final String representation) {
-		return representation.contains(" follows ");
+		return representation.contains(TOKEN);
 	}
 }
