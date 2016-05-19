5cca5dde3ce06db4bffd15f386fb10de14c1e10f
alvaro garcia <derecerca@gmail.com>
2016-05-19 22:02:56 +0200

    refactor: remove unused code

--- src/main/java/com/codurance/test/quakker/CliQuakker.java
+++ src/main/java/com/codurance/test/quakker/CliQuakker.java
@@ -3,10 +3,6 @@ package com.codurance.test.quakker;
 public class CliQuakker {
 	private final Commands commands;
 
-	public CliQuakker (final QuakkRepository repository) {
-		this(repository, Output.MISSING());
-	}
-
 	public CliQuakker (final QuakkRepository repository, final Output output) {
 		commands = new Commands(repository, output);
 	}
