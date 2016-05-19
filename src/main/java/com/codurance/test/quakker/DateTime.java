867025e049a30889e133ef4b53dec6e08f038489
alvaro garcia <derecerca@gmail.com>
2016-05-19 22:37:33 +0200

    green

--- src/main/java/com/codurance/test/quakker/DateTime.java
+++ src/main/java/com/codurance/test/quakker/DateTime.java
@@ -1,6 +1,44 @@
 package com.codurance.test.quakker;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateTime {
-	public DateTime (final String s) {
+	private final Date value;
+
+	public DateTime (final String representation) {
+		try {
+			value = new SimpleDateFormat("HH:MM").parse(representation);
+		} catch (ParseException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static DateTime now () {
+		return null;
+	}
+
+	public String toString () {
+		final StringBuffer sb = new StringBuffer("DateTime{");
+		sb.append("value=").append(value);
+		sb.append('}');
+		return sb.toString();
+	}
+
+	@Override
+	public boolean equals (final Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		final DateTime dateTime = (DateTime) o;
+
+		return value != null ? value.equals(dateTime.value) : dateTime.value == null;
+
+	}
+
+	@Override
+	public int hashCode () {
+		return value != null ? value.hashCode() : 0;
 	}
 }
--- src/main/java/com/codurance/test/quakker/Quakk.java
+++ src/main/java/com/codurance/test/quakker/Quakk.java
@@ -3,10 +3,26 @@ package com.codurance.test.quakker;
 public class Quakk {
 	private final String message;
 	private final User owner;
+	private final DateTime dateTime;
 
 	public Quakk (final String message, final User owner) {
+		this(message, owner, DateTime.now());
+	}
+
+	public Quakk (final String message, final User owner, final DateTime dateTime) {
 		this.message = message;
 		this.owner = owner;
+		this.dateTime = dateTime;
+	}
+
+	@Override
+	public String toString () {
+		final StringBuffer sb = new StringBuffer("Quakk{");
+		sb.append("message='").append(message).append('\'');
+		sb.append(", owner=").append(owner);
+		sb.append(", dateTime=").append(dateTime);
+		sb.append('}');
+		return sb.toString();
 	}
 
 	@Override
@@ -17,7 +33,8 @@ public class Quakk {
 		final Quakk quakk = (Quakk) o;
 
 		if (message != null ? !message.equals(quakk.message) : quakk.message != null) return false;
-		return owner != null ? owner.equals(quakk.owner) : quakk.owner == null;
+		if (owner != null ? !owner.equals(quakk.owner) : quakk.owner != null) return false;
+		return dateTime != null ? dateTime.equals(quakk.dateTime) : quakk.dateTime == null;
 
 	}
 
@@ -25,13 +42,7 @@ public class Quakk {
 	public int hashCode () {
 		int result = message != null ? message.hashCode() : 0;
 		result = 31 * result + (owner != null ? owner.hashCode() : 0);
+		result = 31 * result + (dateTime != null ? dateTime.hashCode() : 0);
 		return result;
-	}
-
-	@Override
-	public String toString () {
-		final StringBuffer sb = new StringBuffer("Quakk{");
-		sb.append('}');
-		return sb.toString();
 	}
 }
