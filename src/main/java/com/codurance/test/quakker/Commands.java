689da1ee69d096a2f5984a9ff133d1040eba5b11
alvaro garcia <derecerca@gmail.com>
2016-05-19 22:27:35 +0200

    refactor: DRY

--- src/main/java/com/codurance/test/quakker/Commands.java
+++ src/main/java/com/codurance/test/quakker/Commands.java
@@ -36,6 +36,7 @@ public class Commands {
 
 	private class Posting implements Rule {
 
+		public static final String KEYWORD_CREATION = " -> ";
 		private final QuakkRepository repository;
 
 		public Posting (final QuakkRepository repository) {
@@ -49,7 +50,7 @@ public class Commands {
 		}
 
 		private Quakk parseQuack (final String representation) {
-			final String[] parts = representation.split(" -> ");
+			final String[] parts = representation.split(KEYWORD_CREATION);
 			final User user = new User(parts[0]);
 			final Quakk quakk = new Quakk(parts[1], user);
 			return quakk;
@@ -57,7 +58,7 @@ public class Commands {
 
 		@Override
 		public boolean appliesTo (final String representation) {
-			return representation.contains("->");
+			return representation.contains(KEYWORD_CREATION);
 		}
 	}
 
