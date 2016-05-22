9d12c48e790e5d074e6b124cc9518b6aaedf3c4d
alvaro garcia <derecerca@gmail.com>
2016-05-22 23:13:01 +0200

    refactor: replace implementation for source of clock

--- src/main/java/com/codurance/test/quakker/core/adapters/SystemClock.java
+++ src/main/java/com/codurance/test/quakker/core/adapters/SystemClock.java
@@ -3,9 +3,13 @@ package com.codurance.test.quakker.core.adapters;
 import com.codurance.test.quakker.core.domain.DateTime;
 import com.codurance.test.quakker.core.ports.Clock;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
 public class SystemClock implements Clock{
 	@Override
 	public DateTime now () {
-		return DateTime.now();
+		return new DateTime(new SimpleDateFormat("H:m:s").format(Date.from(Instant.now())));
 	}
 }
