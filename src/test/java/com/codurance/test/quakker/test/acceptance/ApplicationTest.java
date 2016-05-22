e5cc2422311429d868687a84f4566cb7ab502762
alvaro garcia <derecerca@gmail.com>
2016-05-23 01:38:06 +0200

    red: read commands until finding the keyword

--- src/test/java/com/codurance/test/quakker/test/acceptance/ApplicationTest.java
+++ src/test/java/com/codurance/test/quakker/test/acceptance/ApplicationTest.java
@@ -48,4 +48,29 @@ public class ApplicationTest {
 
         context.assertIsSatisfied();
     }
+
+    @Test
+    public void should_execute_commands_until_finding_the_keyword() {
+
+        context.checking(new Expectations() {{
+            exactly(2).of(input).read(); will(onConsecutiveCalls(
+                    returnValue("John -> first quakk!"),
+                    returnValue("John wall"),
+                    returnValue("John wall"),
+                    returnValue("Stop!")
+            ));
+            allowing(clock).now(); will(onConsecutiveCalls(
+                    returnValue(new DateTime("21:28")),
+                    returnValue(new DateTime("21:30")),
+                    returnValue(new DateTime("21:31"))
+                    ));
+            oneOf(output).print("John - first quakk! (2 minutes ago)");
+            oneOf(output).print("John - first quakk! (3 minutes ago)");
+        }});
+        final CLIQuakkerClient cli = new CLIQuakkerClient(repository, output, clock, input);
+
+        cli.run();
+
+        context.assertIsSatisfied();
+    }
 }
