35b4241308b9294656edcb5602945c32e4bd3576
alvaro garcia <derecerca@gmail.com>
2016-05-23 00:11:10 +0200

    green: add test for wall format

---
+++ src/test/java/com/codurance/test/quakker/core/adapters/WallFormatShould.java
@@ -0,0 +1,45 @@
+package com.codurance.test.quakker.core.adapters;
+
+import com.codurance.test.quakker.core.domain.DateTime;
+import com.codurance.test.quakker.core.domain.Quakk;
+import com.codurance.test.quakker.core.domain.User;
+import com.codurance.test.quakker.core.ports.Clock;
+import com.codurance.test.quakker.core.ports.Format;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class WallFormatShould {
+
+	private Mockery context;
+	private Clock clock;
+	private Format format;
+
+	@Before
+	public void setUp () {
+		context = new Mockery();
+		clock = context.mock(Clock.class);
+	}
+
+	@Test
+	public void format_quakks () {
+
+		format = new WallFormat(clock);
+
+		context.checking(new Expectations() {{
+			oneOf(clock).now(); will(returnValue(new DateTime("21:10")));
+		}});
+
+		final String formatted = format.format(Quakk.QuakkBuilder.aNew("message").from(new User("user")).at(new DateTime("21:00"))
+				.build());
+
+		assertThat(formatted, is("user - message (10 minutes ago)"));
+
+		context.assertIsSatisfied();
+	}
+
+}
