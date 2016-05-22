da5a33cc8b163e36d445361175c0500ae144ff80
alvaro garcia <derecerca@gmail.com>
2016-05-23 00:32:14 +0200

    green

--- src/main/java/com/codurance/test/quakker/core/adapters/DateDifferenceFormatter.java
+++ src/main/java/com/codurance/test/quakker/core/adapters/DateDifferenceFormatter.java
@@ -2,8 +2,14 @@ package com.codurance.test.quakker.core.adapters;
 
 import com.codurance.test.quakker.core.domain.DateTime;
 
+import java.time.Duration;
+
 public class DateDifferenceFormatter {
 	public String difference (final DateTime start, final DateTime end) {
+		final Duration duration = end.minus(start);
+		if (duration.toMillis() > 1000) {
+			return String.format("%d seconds ago", duration.toMillis() / 1_000);
+		}
 		return "1 second ago";
 	}
 }
