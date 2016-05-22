3c0aeb6ab0789c423a26023425dbd6400508a670
alvaro garcia <derecerca@gmail.com>
2016-05-23 01:31:41 +0200

    green

--- src/main/java/com/codurance/test/quakker/CLIQuakkerClient.java
+++ src/main/java/com/codurance/test/quakker/CLIQuakkerClient.java
@@ -1,21 +1,19 @@
 package com.codurance.test.quakker;
 
+import com.codurance.test.quakker.application.QuakkerClient;
 import com.codurance.test.quakker.core.ports.Clock;
 import com.codurance.test.quakker.core.ports.Output;
 import com.codurance.test.quakker.core.ports.QuakkRepository;
 
 public class CLIQuakkerClient {
-    private final QuakkRepository repository;
-    private final Output output;
-    private final Clock clock;
+
+    private final QuakkerClient client;
 
     public CLIQuakkerClient (final QuakkRepository repository, final Output output, final Clock clock) {
-        this.repository = repository;
-        this.output = output;
-        this.clock = clock;
+        this.client = new QuakkerClient(repository, output, clock);
     }
 
     public void execute (final String commandRepresentation) {
-
+        this.client.execute(commandRepresentation);
     }
 }
