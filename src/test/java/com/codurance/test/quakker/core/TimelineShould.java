597e7ea2fa855d59fe680777574450ff96e50247
alvaro garcia <derecerca@gmail.com>
2016-05-22 18:27:52 +0200

    red: sort the timeline on creation as well

--- src/test/java/com/codurance/test/quakker/core/TimelineShould.java
+++ src/test/java/com/codurance/test/quakker/core/TimelineShould.java
@@ -23,4 +23,15 @@ public class TimelineShould {
 		assertThat(new Timeline(quakk1, quakk3).merge(new Timeline(quakk2)), is(new Timeline(quakk1, quakk2, quakk3)));
 	}
 
+
+	@Test
+	public void the_timeline_is_sorted_by_date () {
+
+		final Quakk quakk1 = Quakk.QuakkBuilder.aNew("quakk1").from(ANY_USER).at(new DateTime("21:00")).build();
+		final Quakk quakk2 = Quakk.QuakkBuilder.aNew("quakk2").from(ANY_USER).at(new DateTime("22:00")).build();
+		final Quakk quakk3 = Quakk.QuakkBuilder.aNew("quakk3").from(ANY_USER).at(new DateTime("22:00")).build();
+
+		assertThat(new Timeline(quakk2, quakk1, quakk3), is(new Timeline(quakk1, quakk2, quakk3)));
+	}
+
 }
