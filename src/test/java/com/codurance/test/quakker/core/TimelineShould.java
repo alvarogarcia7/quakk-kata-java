cbd62d6ef462a4b1887743a1822933fc0742c4d6
alvaro garcia <derecerca@gmail.com>
2016-05-22 18:32:36 +0200

    refactor: remove duplication

--- src/test/java/com/codurance/test/quakker/core/TimelineShould.java
+++ src/test/java/com/codurance/test/quakker/core/TimelineShould.java
@@ -16,22 +16,26 @@ public class TimelineShould {
 	@Test
 	public void merging_two_timelines_sorts_by_date () {
 
-		final Quakk quakk1 = Quakk.QuakkBuilder.aNew("quakk1").from(ANY_USER).at(new DateTime("21:00")).build();
-		final Quakk quakk2 = Quakk.QuakkBuilder.aNew("quakk2").from(ANY_USER).at(new DateTime("22:00")).build();
-		final Quakk quakk3 = Quakk.QuakkBuilder.aNew("quakk3").from(ANY_USER).at(new DateTime("23:00")).build();
+		final Quakk quakk1 = aQuakk("quakk1", "21:00");
+		final Quakk quakk2 = aQuakk("quakk2", "22:00");
+		final Quakk quakk3 = aQuakk("quakk3", "23:00");
 
 		assertThat(new Timeline(quakk1, quakk3).merge(new Timeline(quakk2)), is(new Timeline(quakk1, quakk2, quakk3)));
 	}
 
-
 	@Test
 	public void the_timeline_is_sorted_by_date () {
 
-		final Quakk quakk1 = Quakk.QuakkBuilder.aNew("quakk1").from(ANY_USER).at(new DateTime("21:00")).build();
-		final Quakk quakk2 = Quakk.QuakkBuilder.aNew("quakk2").from(ANY_USER).at(new DateTime("22:00")).build();
-		final Quakk quakk3 = Quakk.QuakkBuilder.aNew("quakk3").from(ANY_USER).at(new DateTime("23:00")).build();
+		final Quakk quakk1 = aQuakk("quakk1", "21:00");
+		final Quakk quakk2 = aQuakk("quakk2", "22:00");
+		final Quakk quakk3 = aQuakk("quakk3", "23:00");
 
 		assertThat(new Timeline(quakk2, quakk1, quakk3), is(new Timeline(quakk1, quakk2, quakk3)));
+	}
+
+
+	private Quakk aQuakk (final String message, final String time) {
+		return Quakk.QuakkBuilder.aNew(message).from(ANY_USER).at(new DateTime(time)).build();
 	}
 
 }
