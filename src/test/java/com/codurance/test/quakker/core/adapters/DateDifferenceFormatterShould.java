90f41af285dc515735ec5f47508c39cc6ea46415
alvaro garcia <derecerca@gmail.com>
2016-05-23 00:24:57 +0200

    refactor: extract field from test

--- src/test/java/com/codurance/test/quakker/core/adapters/DateDifferenceFormatterShould.java
+++ src/test/java/com/codurance/test/quakker/core/adapters/DateDifferenceFormatterShould.java
@@ -1,6 +1,7 @@
 package com.codurance.test.quakker.core.adapters;
 
 import com.codurance.test.quakker.core.domain.DateTime;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,9 +10,15 @@ import static org.hamcrest.Matchers.is;
 public class DateDifferenceFormatterShould {
 
 
+	private DateDifferenceFormatter dateDifferenceFormatter;
+
+	@Before
+	public void setUp () throws Exception {
+		dateDifferenceFormatter = new DateDifferenceFormatter();
+	}
+
 	@Test
 	public void calculate_a_single_second_difference () {
-		final DateDifferenceFormatter dateDifferenceFormatter = new DateDifferenceFormatter();
 		final DateTime start = new DateTime("00:00:00");
 		final DateTime end = new DateTime("00:00:01");
 
