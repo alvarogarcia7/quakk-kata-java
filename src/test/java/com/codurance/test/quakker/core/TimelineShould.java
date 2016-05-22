b5f985004eec070ee4cefe4e0f03794fc3db627d
alvaro garcia <derecerca@gmail.com>
2016-05-22 18:33:28 +0200

    refactor: extract to fields

--- src/test/java/com/codurance/test/quakker/core/TimelineShould.java
+++ src/test/java/com/codurance/test/quakker/core/TimelineShould.java
@@ -12,24 +12,17 @@ import static org.hamcrest.core.Is.is;
 public class TimelineShould {
 
 	private static final User ANY_USER = new User("ANY");
+	private final Quakk quakk1 = aQuakk("quakk1", "21:00");
+	private final Quakk quakk2 = aQuakk("quakk2", "22:00");
+	private final Quakk quakk3 = aQuakk("quakk3", "23:00");
 
 	@Test
 	public void merging_two_timelines_sorts_by_date () {
-
-		final Quakk quakk1 = aQuakk("quakk1", "21:00");
-		final Quakk quakk2 = aQuakk("quakk2", "22:00");
-		final Quakk quakk3 = aQuakk("quakk3", "23:00");
-
 		assertThat(new Timeline(quakk1, quakk3).merge(new Timeline(quakk2)), is(new Timeline(quakk1, quakk2, quakk3)));
 	}
 
 	@Test
 	public void the_timeline_is_sorted_by_date () {
-
-		final Quakk quakk1 = aQuakk("quakk1", "21:00");
-		final Quakk quakk2 = aQuakk("quakk2", "22:00");
-		final Quakk quakk3 = aQuakk("quakk3", "23:00");
-
 		assertThat(new Timeline(quakk2, quakk1, quakk3), is(new Timeline(quakk1, quakk2, quakk3)));
 	}
 
