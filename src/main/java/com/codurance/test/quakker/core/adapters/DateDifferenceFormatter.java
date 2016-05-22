e2f68fb0395c9e0c1ab8369f127dc992942e79b7
alvaro garcia <derecerca@gmail.com>
2016-05-23 00:40:08 +0200

    refactor: reorder variables

--- src/main/java/com/codurance/test/quakker/core/adapters/DateDifferenceFormatter.java
+++ src/main/java/com/codurance/test/quakker/core/adapters/DateDifferenceFormatter.java
@@ -8,20 +8,20 @@ public class DateDifferenceFormatter {
 	public String difference (final DateTime start, final DateTime end) {
 		final Duration duration = end.minus(start);
 		final long hours = duration.toHours();
+		final long minutes = duration.toMinutes();
+		final long seconds = duration.toMillis() / 1_000;
 		if (hours > 0) {
 			if (hours > 1) {
 				return String.format("%d hours ago", hours);
 			}
 			return "1 hour ago";
 		}
-		final long minutes = duration.toMinutes();
 		if (minutes > 0) {
 			if (minutes > 1) {
 				return String.format("%d minutes ago", minutes);
 			}
 			return "1 minute ago";
 		}
-		final long seconds = duration.toMillis() / 1_000;
 		if (seconds > 1) {
 			return String.format("%d seconds ago", seconds);
 		}
