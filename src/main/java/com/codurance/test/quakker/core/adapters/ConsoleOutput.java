b6734a5fd2c0a74da1fbb33ecedf148d08363fa9
alvaro garcia <derecerca@gmail.com>
2016-05-23 01:15:23 +0200

    red: console output

---
+++ src/main/java/com/codurance/test/quakker/core/adapters/ConsoleOutput.java
@@ -0,0 +1,16 @@
+package com.codurance.test.quakker.core.adapters;
+
+import com.codurance.test.quakker.core.ports.Output;
+
+import java.io.PrintStream;
+
+public class ConsoleOutput implements Output {
+    public ConsoleOutput (final PrintStream printStream) {
+
+    }
+
+    @Override
+    public void print (final String line) {
+
+    }
+}
---
+++ src/test/java/com/codurance/test/quakker/core/ports/ConsoleOutputShould.java
@@ -0,0 +1,52 @@
+package com.codurance.test.quakker.core.ports;
+
+import com.codurance.test.quakker.core.adapters.ConsoleOutput;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ConsoleOutputShould {
+
+    @Test
+    public void print_to_console () {
+
+        final MockSystemOutput mockSystemOutput = injectSystemOutput();
+
+        new ConsoleOutput(System.out).print("hellso");
+
+        assertThat(mockSystemOutput.toString(), is("hello\\n"));
+
+    }
+
+    private MockSystemOutput injectSystemOutput () {
+        final MockSystemOutput mockOutput = new MockSystemOutput();
+
+        System.setOut(new PrintStream(new OutputStream() {
+
+            @Override
+            public void write (int b) throws IOException {
+                mockOutput.write(b);
+
+            }
+        }));
+        return mockOutput;
+    }
+
+    public class MockSystemOutput {
+        private final StringBuilder stringBuilder = new StringBuilder();
+
+        public void write (int b) {
+            stringBuilder.append((char) b);
+        }
+
+        @Override
+        public String toString () {
+            return stringBuilder.toString();
+        }
+    }
+}
