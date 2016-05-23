7844fac0d8378a40ccffd93d07c99dd0def5ad1a
alvaro garcia <derecerca@gmail.com>
2016-05-24 01:12:20 +0200

    refactor: remove duplication in test

--- src/test/java/com/codurance/test/quakker/test/unit/TimelineShould.java
+++ src/test/java/com/codurance/test/quakker/test/unit/TimelineShould.java
@@ -29,16 +29,8 @@ public class TimelineShould {
 
 	@Test
 	public void two_timelines_are_not_equal_based_on_the_quakks () {
-		final Quakk a = Quakk.QuakkBuilder
-				.aNew("message1")
-				.from(new User("user"))
-				.at(new DateTime("22:00"))
-				.build();
-		final Quakk b = Quakk.QuakkBuilder
-				.aNew("message2")
-				.from(new User("user"))
-				.at(new DateTime("22:00"))
-				.build();
+		final Quakk a = aQuakk("message1", "22:00");
+		final Quakk b = aQuakk("message2", "22:00");
 		assertThat(new Timeline(a), is(not(new Timeline(a,b))));
 	}
 
